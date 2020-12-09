9318db1360a4f1b8aba70c4f8acff4c69365d209
Felippe <felippeotaviorosa@gmail.com>
2020-12-09 19:56:25 -0300

    StackTest

--- src/Stack.java
+++ src/Stack.java
@@ -14,43 +14,45 @@ public class Stack {
         }
     }
 
-    public Stack(Node topo) {
+    public Stack() {
         topo = null;
-    }
-
-    public Node getTopo() {
-        return topo;
-    }
-
-    public void setTopo(Node topo) {
-        this.topo = topo;
-    }
-
-    public void setTamanho(int tamanho) {
-        this.tamanho = tamanho;
     }
 
     //empilhar
     public void push(int data) {
-        // criar a casinha
-        Node aux = topo;
-        topo = new Node(data);
-        topo.next = aux;
+        // se a pilha estiver vazia
+        if (isEmpty()){
+            Node aux = new Node(data);
+            topo = aux;
+        }
+        else {
+            Node aux = new Node(data);
+            aux.next = topo;
+            topo = aux;
+        }
         tamanho++;
     }
 
     // desenfileirar
     public int pop() {
 
+        //T pop();
+        //não pode ser vazia!!!
+        //aux = topo
+        // topo aponta para aux.next, aux.next < -null
+        //retorna  aux.valor
+
         int data ; // valor retornado ao usuário
 
-        if (isEmpty()) { // não pode desenfileirar! erro!
-            throw new IndexOutOfBoundsException("Pilha vazia! Impossível desenpilhar!");
+        if (isEmpty()) { // não pode desempilhar! erro!
+            throw new IndexOutOfBoundsException("Pilha vazia! Impossível desempilhar!");
         }
 
-        data = topo.data;  // etiqueta auxiliar aponta para o primeiro nó
-        topo = topo.next; // primeiro aponta para o segundo
+        Node aux = topo;
+        topo = aux.next; // primeiro aponta para o segundo
+        aux.next = null;
         tamanho--;
+        data = aux.data;
 
         return data;
     }
@@ -64,10 +66,12 @@ public class Stack {
     }
 
         //void push(data);
-        // se a pilha estiver vazia
-        //cria casinha e opo <- casinha
-        //senão
-        // cria casinha, casinha.next = topo, topo = casinha
+
+    //cria casinha e opo <- casinha
+    //senão
+    // cria casinha, casinha.next = topo, topo = casinha
+    // criar a casinha
+
 
         //T pop();
         //não pode ser vazia!!!
--- src/StackTest.java
+++ src/StackTest.java
@@ -1,8 +1,22 @@
-import java.util.Scanner;
-
 public class StackTest {
     public static void main(String[] args) {
-        Stack q;
+        Stack s = new Stack();
 
+        s.push(125);
+        s.push(555);
+        s.push(677);
+        s.push(654);
+
+        try {
+            System.out.println(s.pop());
+            System.out.println(s.pop());
+            System.out.println(s.pop());
+            System.out.println(s.pop());
+            System.out.println(s.pop());
+            // Erro! fila vazia
+        } catch (IndexOutOfBoundsException ex){
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Fim do Programa!");
     }
 }
